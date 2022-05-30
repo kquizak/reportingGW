17e8388b9aa6fbaa2a78771a4aec1f53d93478c0
kquizak <kquizak@github.com>
2022-05-30 12:11:23 +0000

    ceva8

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
 app = Flask(__name__)
 
 
